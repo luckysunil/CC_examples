8a695cb77f4ae89150acc1d103a12cec5450d71a
Sunil Kumar Behera <sbehera@confluent.io>
2022-01-10 15:28:10 +0530

    Added Sample code for Complex type schemas

--- avro/serdes_sample.py
+++ avro/serdes_sample.py
@@ -7,20 +7,32 @@ schema = avro.schema.parse(open("sample_schema.avsc", "rb").read())
 
 writer = DataFileWriter(open("sample_schema.avro", "wb"), DatumWriter(), schema)
 
+sample_array_data = ["One String","Two String"]
+
 writer.append({
                 "sample_string":"Sunil",
                 "sample_int":0,
                 "sample_float":100.50,
                 "sample_boolean":True,
                 "sample_string_union":"Kumar",
-                "sample_int_union":100
+                "sample_int_union":100,
+                "sample_nested_record":{
+                    "sample_nested_string":"Nested String Data",
+                    "sample_nested_int":10
+                    },
+                "sample_array":sample_array_data
                 })
 
 writer.append({
                 "sample_string":"Jasmine",
                 "sample_int":1,
-                "sample_float":100.50,
-                "sample_boolean":True,
+                "sample_float":1.5,
+                "sample_boolean":False,
+                "sample_nested_record":{
+                    "sample_nested_string":"Nested String Data",
+                    "sample_nested_int":10
+                    },
+                "sample_array":sample_array_data
                 })
 
 writer.close()
@@ -34,7 +46,8 @@ reader.close()
 """
 
 # Sample Output
-{'sample_string': 'Sunil', 'sample_int': 0, 'sample_float': 100.5, 'sample_boolean': True, 'sample_string_union': 'Kumar', 'sample_int_union': 100}
-{'sample_string': 'Jasmine', 'sample_int': 1, 'sample_float': 100.5, 'sample_boolean': True, 'sample_string_union': None, 'sample_int_union': None}
+{'sample_string': 'Sunil', 'sample_int': 0, 'sample_float': 100.5, 'sample_boolean': True, 'sample_string_union': 'Kumar', 'sample_int_union': 100, 'sample_nested_record': {'sample_nested_string': 'Nested String Data', 'sample_nested_int': 10}, 'sample_array': ['One String', 'Two String']}
+
+{'sample_string': 'Jasmine', 'sample_int': 1, 'sample_float': 1.5, 'sample_boolean': False, 'sample_string_union': None, 'sample_int_union': None, 'sample_nested_record': {'sample_nested_string': 'Nested String Data', 'sample_nested_int': 10}, 'sample_array': ['One String', 'Two String']}
 
 """
